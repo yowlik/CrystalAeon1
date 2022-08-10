3bba3a0009cfdf8d88a0d98080f9f4892278e9a0
yowlik <yowl609@gmail.com>
2022-08-10 11:44:46 +0300

    test
    test

---
+++ app/src/main/java/com/lt/latte/bric/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.lt.latte.bric.black
+
+object CNST {
+    const val AF_DEV_KEY = "2gRxH28VpgJh4rKAN2E99S"
+    const val jsoupCheck = "4g7f"
+    const val ONESIGNAL_APP_ID = "abaf7af7-150d-41f2-ab83-928ff0b4aca7"
+
+    var lru = "http://crystalaeon.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
